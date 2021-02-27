f467d7ff8dfbde52671179d6a6af7dd9d16c98c0
belleau <pascal_belleau@hotmail.com>
2021-02-27 12:39:19 -0500

    selectSNV run for chr 1 to 22

--- scripts/selectSNV.R
+++ scripts/selectSNV.R
@@ -6,21 +6,22 @@ load_all(path = "/mnt/wigclust18/data/unsafe/belleau/beatHaplo/subsetMix/package
 
 sampleRef <- argv[1]
 nbSim <- as.numeric(argv[2])
-chr <- argv[3]
-minFreq <- argv[4]
+minFreq <- argv[3]
 
 nameSel <- gsub(".rds", "", sampleRef)
 
 
 PATH_1K <- "/mnt/wigclust5/data/unsafe/belleau/process1000G/samples1000gUnrelated/"
-PATH_OUT <- paste0("data/simRes.", nameSel, ".", nbSim,"/", chr, "/")
+for(c in seq_len(22)){
+    PATH_OUT <- paste0("data/simRes.", nameSel, ".", nbSim,"/", chr, "/")
 
-fileMatFreq <- paste0(PATH_1K, "genotypeSample/", chr, "/matFreqSNV", chr,".txt.bz2")
-matFreq <- read.csv2(fileMatFreq, header=FALSE)
-colnames(matFreq) <- c("chr", "pos", "ref", "alt", "AF", "EAS_AF" ,"EUR_AF", "AFR_AF", "AMR_AF", "SAS_AF")
+    fileMatFreq <- paste0(PATH_1K, "genotypeSample/", chr, "/matFreqSNV", chr,".txt.bz2")
+    matFreq <- read.csv2(fileMatFreq, header=FALSE)
+    colnames(matFreq) <- c("chr", "pos", "ref", "alt", "AF", "EAS_AF" ,"EUR_AF", "AFR_AF", "AMR_AF", "SAS_AF")
 
-infoSNV <- parseSelMinFreq(snv=matFreq, genotype=genotype, minFreq)
+    infoSNV <- parseSelMinFreq(snv=matFreq, genotype=genotype, minFreq)
 
-saveRDS(infoSNV, paste0(PATH_OUT, "infoSNV.f", minFreq,".rds"))
+    saveRDS(infoSNV, paste0(PATH_OUT, "infoSNV.f", minFreq,".rds"))
+}
 
 q()
